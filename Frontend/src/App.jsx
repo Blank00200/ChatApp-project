0a1e4928eb5e711add30976ba528cb6b773b5ef3
Blank00200 <raghavraturi8c@gmail.com>
2024-09-16 09:26:37 +0100

    Monday 16

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,22 +1,22 @@
 import React from "react";
-import Left from "./home/Leftpart/Left";
-import Right from "./home/Rightpart/Right";
+// import Left from "./home/Leftpart/Left";
+// import Right from "./home/Rightpart/Right";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 
 function App() {
   return (
-    <div className="home" style={{ display: 'flex', height: '100vh', width: '100vw' }}>
-      <div style={{ flexBasis: "30%", backgroundColor: "red", color: "#D1D5DB" }}>
-        <Left />
-      </div>
-      <div style={{ flexBasis: "70%", backgroundColor: "green", color: "#D1D5DB" }}>
-        <Right />
-      </div>
-    </div>
+    // <div className="home" style={{ display: 'flex', height: '100vh', width: '100vw' }}>
+    //   <div style={{ flexBasis: "30%", backgroundColor: "red", color: "#D1D5DB" }}>
+    //     <Left />
+    //   </div>
+    //   <div style={{ flexBasis: "70%", backgroundColor: "green", color: "#D1D5DB" }}>
+    //     <Right />
+    //   </div>
+    // </div>
+    // <Login />
+    <Signup />
   );
 }
-  // <Login />
-  // <Signup />
 
 export default App;
