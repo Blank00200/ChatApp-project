e92e4bbfe67ef4a566722c38095a2ddc142020b8
Blank00200 <raghavraturi8c@gmail.com>
2024-11-24 14:01:20 +0000

    Update Left.jsx

--- Backend/Frontend/src/home/Leftpart/Left.jsx
+++ Backend/Frontend/src/home/Leftpart/Left.jsx
@@ -5,18 +5,18 @@ import Logout from "./Logout";
 
 function Left() {
   return (
-    <div className="w-full bg-black text-gray-300 flex flex-col h-full">
-      {/* Search Bar */}
+    <div className="w-full lg:w-80 bg-black text-gray-300 flex flex-col h-screen">
+      {/* Search Section */}
       <div className="p-4">
         <Search />
       </div>
 
       {/* Users List */}
-      <div className="flex-1 overflow-y-auto p-2 lg:p-4" style={{ minHeight: "calc(84vh - 10vh)" }}>
+      <div className="flex-1 overflow-y-auto p-2">
         <Users />
       </div>
 
-      {/* Logout Button */}
+      {/* Logout Section */}
       <div className="p-4 border-t border-gray-700">
         <Logout />
       </div>
